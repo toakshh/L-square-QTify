5fcf9b85c3417edde6a40f9a4cb40e5ff64032c3
Aksh <toakshh@gmail.com>
2023-07-07 22:59:05 +0530

    hero section added

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Hero from './components/body/Hero';
 import Navbar from './components/header/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ qtify/src/components/body/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {ReactComponent as HeroImage} from "../assets/vibratingheadphone.svg";
+import "./Hero.css"
+import { Grid } from '@mui/material';
+
+const Hero = () => {
+  return (
+    <Grid container className='hero-main'>
+        <h1 className='hero-text'>
+            <div>100 Thousand Songs, ad free</div>
+            <div>Over thousands podcast episodes</div>
+        </h1>
+        <HeroImage />
+    </Grid>
+  )
+}
+
+export default Hero
